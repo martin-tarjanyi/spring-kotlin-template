b2c6b19a4ac93db9892b8ac4539a9cd399fecccf
Martin Tarjányi <17810336+martin-tarjanyi@users.noreply.github.com>
2024-03-02 15:42:21 +0100

    Remove toolchain to speed up build

--- buildSrc/src/main/kotlin/kotlin-module-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-module-conventions.gradle.kts
@@ -16,9 +16,6 @@ repositories {
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
 }
 
 tasks.withType<KotlinCompile> {
