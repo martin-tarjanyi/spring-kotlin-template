095df83e497414b0882bab7d2b66d1b77148c33d
Martin Tarjányi <17810336+martin-tarjanyi@users.noreply.github.com>
2024-03-03 21:41:00 +0100

    Add test for access log

--- backend/todo-web/app/src/integrationTest/kotlin/com/example/product/ObservabilityWebIntegrationTest.kt
+++ backend/todo-web/app/src/integrationTest/kotlin/com/example/product/ObservabilityWebIntegrationTest.kt
@@ -2,11 +2,19 @@ package com.example.product
 
 import com.example.product.logging.LogTestListener
 import io.kotest.inspectors.shouldForOne
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.string.shouldNotBeBlank
+import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.web.reactive.server.expectBody
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.toEntity
 
 class ObservabilityWebIntegrationTest : BaseWebIntegrationTest() {
+    @LocalServerPort
+    private var port: Int = 0
+
     init {
         val logListener = LogTestListener()
         listener(logListener)
@@ -28,6 +36,20 @@ class ObservabilityWebIntegrationTest : BaseWebIntegrationTest() {
                     }
                     .shouldForOne { event ->
                         event.formattedMessage.shouldContain("After delay")
+                        event.mdcPropertyMap["traceId"].shouldContain(traceId)
+                    }
+            }
+
+            should("access log") {
+                // Access log is not logged for some reason with WebTestClient, so we use a real one here
+                val result = WebClient.create("http://localhost:$port").get().uri("/log")
+                    .retrieve().toEntity<String>().awaitSingle()
+
+                val traceId = result.headers["traceid"]?.firstOrNull().shouldNotBeBlank()!!
+
+                logListener.events
+                    .shouldForOne { event ->
+                        event.loggerName shouldBe "ACCESS_LOG"
                         event.mdcPropertyMap["traceId"].shouldContain(traceId)
                     }
             }
