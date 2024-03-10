35add39c9db7e2d0e19c4bf4f0bbd620aa3b2320
Martin Tarjányi <tmartin193@gmail.com>
2024-03-10 20:22:15 +0100

    Customize logstash JSON logging (#19)

--- backend/todo-common/util/logging/build.gradle.kts
+++ backend/todo-common/util/logging/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 dependencies {
     implementation("ch.qos.logback:logback-classic")
+    implementation("org.mongodb:bson")
     implementation(libs.logstash.encoder)
     implementation(libs.loki.logback)
 }
